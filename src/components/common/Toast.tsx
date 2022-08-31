d7d2b984a50d95c51df545509543d65eb8606ab7
happyeveryone96 <happyeveryone@naver.com>
2022-08-31 19:15:50 +0900

    chore: code formatting

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -25,19 +25,19 @@ const Background = styled.div`
 `;
 
 const fadeIn = keyframes`
-from {
-  opacity: 0; 
-} to {
-  opacity: 1;
-}
+  from {
+    opacity: 0; 
+  } to {
+    opacity: 1;
+  }
 `;
 
 const fadeOut = keyframes`
-from {
-  opacity: 1; 
-} to {
-  opacity: 0;
-}
+  from {
+    opacity: 1; 
+  } to {
+    opacity: 0;
+  }
 `;
 
 const ToastBox = styled.div`
